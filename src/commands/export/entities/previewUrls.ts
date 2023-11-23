2e6691e377ac25c8052cc57c1c86492e35f24165
Ivan Kiral <kiralivan@gmail.com>
2023-11-23 10:11:34 +0100

    add preview urls to export

---
+++ src/commands/export/entities/previewUrls.ts
@@ -0,0 +1,9 @@
+import { PreviewContracts } from "@kontent-ai/management-sdk";
+
+import { EntityDefinition } from "../entityDefinition.js";
+
+export const previewUrlsExportEntity: EntityDefinition<PreviewContracts.IPreviewConfigurationContract> = {
+    name: "previewUrls",
+    fetchEntities: client => client.getPreviewConfiguration().toPromise().then(res => res.rawData),
+    serializeEntities: collections => JSON.stringify(collections),
+  };
