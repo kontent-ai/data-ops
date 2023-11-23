dd922937d5e728e6c74e2b8bf498edca66400c82
Ivan Kiral <kiralivan@gmail.com>
2023-11-23 10:15:42 +0100

    add content types into export

---
+++ src/commands/export/entities/contentTypes.ts
@@ -0,0 +1,12 @@
+import { ContentTypeContracts } from "@kontent-ai/management-sdk";
+
+import { EntityDefinition } from "../entityDefinition.js";
+
+export const contentTypesExportEntity: EntityDefinition<ReadonlyArray<ContentTypeContracts.IContentTypeContract>> = {
+  name: "contentTypes",
+  fetchEntities: client => client
+    .listContentTypes()
+    .toAllPromise()
+    .then(res => res.data.items.map(t => t._raw)),
+  serializeEntities: collections => JSON.stringify(collections),
+};
