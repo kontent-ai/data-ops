68d4ac9074e7415e1ba9ccf301159520d65b1447
Jiri Lojda <lojdaj@seznam.cz>
2023-12-06 12:23:18 +0100

    Add taxonomy ids into context

--- src/commands/importExportEntities/entities/taxonomies.ts
+++ src/commands/importExportEntities/entities/taxonomies.ts
@@ -1,5 +1,6 @@
 import { TaxonomyContracts } from "@kontent-ai/management-sdk";
 
+import { zip } from "../../../utils/array.js";
 import { serially } from "../../../utils/requests.js";
 import { EntityDefinition } from "../entityDefinition.js";
 
@@ -7,12 +8,21 @@ export const taxonomiesEntity: EntityDefinition<ReadonlyArray<TaxonomyContracts.
   name: "taxonomies",
   fetchEntities: client => client.listTaxonomies().toAllPromise().then(res => res.data.items.map(t => t._raw)),
   serializeEntities: taxonomies => JSON.stringify(taxonomies),
-  importEntities: async (client, fileTaxonomies) => {
-    await serially(fileTaxonomies.map(taxonomy => () =>
-      client
-        .addTaxonomy()
-        .withData(addExternalIds(taxonomy))
-        .toPromise()));
+  importEntities: async (client, fileTaxonomies, context) => {
+    const projectTaxonomies = await serially<ReadonlyArray<() => Promise<TaxonomyContracts.ITaxonomyContract>>>(
+      fileTaxonomies.map(taxonomy => () =>
+        client
+          .addTaxonomy()
+          .withData(addExternalIds(taxonomy))
+          .toPromise()
+          .then(res => res.data._raw))
+    );
+
+    return {
+      ...context,
+      taxonomyGroupIdsByOldIds: new Map(zip(fileTaxonomies.map(t => t.id), projectTaxonomies.map(t => t.id))),
+      taxonomyTermIdsByOldIds: new Map(zip(fileTaxonomies.flatMap(t => t.terms), projectTaxonomies.flatMap(t => t.terms)).flatMap(extractTermIdsEntries)),
+    };
   },
   deserializeEntities: JSON.parse,
 };
@@ -22,3 +32,8 @@ const addExternalIds = (taxonomy: TaxonomyContracts.ITaxonomyContract): Taxonomy
   external_id: taxonomy.external_id ?? taxonomy.codename,
   terms: taxonomy.terms.map(addExternalIds),
 });
+
+const extractTermIdsEntries = ([fileTaxonomy, projectTaxonomy]: readonly [TaxonomyContracts.ITaxonomyContract, TaxonomyContracts.ITaxonomyContract]): ReadonlyArray<readonly [string, string]> => [
+  [fileTaxonomy.id, projectTaxonomy.id] as const,
+  ...zip(fileTaxonomy.terms, projectTaxonomy.terms).flatMap(extractTermIdsEntries),
+];
