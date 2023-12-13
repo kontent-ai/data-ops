ba50e12cd24345222eee3cdf2e100a797b976ceb
Jiri Lojda <lojdaj@seznam.cz>
2023-12-13 14:37:45 +0100

    Add support for importing items

--- src/commands/importExportEntities/entities/contentItems.ts
+++ src/commands/importExportEntities/entities/contentItems.ts
@@ -1,12 +1,36 @@
-import { ContentItemContracts } from "@kontent-ai/management-sdk";
+import { ContentItemContracts, ManagementClient } from "@kontent-ai/management-sdk";
 
-import { EntityDefinition } from "../entityDefinition.js";
+import { zip } from "../../../utils/array.js";
+import { serially } from "../../../utils/requests.js";
+import { EntityDefinition, ImportContext } from "../entityDefinition.js";
 
 export const contentItemsExportEntity: EntityDefinition<ReadonlyArray<ContentItemContracts.IContentItemModelContract>> =
   {
     name: "contentItems",
     fetchEntities: client => client.listContentItems().toAllPromise().then(res => res.data.items.map(i => i._raw)),
     serializeEntities: collections => JSON.stringify(collections),
-    importEntities: () => Promise.resolve(),
     deserializeEntities: JSON.parse,
+    importEntities: async (client, fileItems, context) => {
+      const projectItems = await serially(fileItems.map(createImportItemFetcher(client, context)));
+
+      return {
+        ...context,
+        contentItemIdsByOldIds: new Map(zip(fileItems, projectItems).map(([fItem, pItem]) => [fItem.id, pItem.id])),
+      };
+    },
   };
+
+const createImportItemFetcher =
+  (client: ManagementClient, context: ImportContext) =>
+  (fileItem: ContentItemContracts.IContentItemModelContract) =>
+  () =>
+    client
+      .addContentItem()
+      .withData({
+        ...fileItem,
+        type: { id: context.contentTypeIdsWithElementsByOldIds.get(fileItem.type.id ?? "")?.selfId },
+        collection: { id: context.collectionIdsByOldIds.get(fileItem.collection.id ?? "") },
+        external_id: fileItem.external_id ?? fileItem.codename,
+      })
+      .toPromise()
+      .then(res => res.rawData);
