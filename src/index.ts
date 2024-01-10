4cf9c909c8ea5b3e48ae9d5364cf42e1db623d74
Jiri Lojda <lojdaj@seznam.cz>
2024-01-10 12:13:35 +0100

    Improve help text

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import chalk from "chalk";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
@@ -10,16 +11,21 @@ const commandsToRegister: ReadonlyArray<RegisterCommand> = [
   (await import("./commands/import.js")).register,
 ];
 
-const initialYargs = yargs(hideBin(process.argv)) // hides the first two arguments - path to script and path to node.js
+const emptyYargs = yargs(hideBin(process.argv)); // hides the first two arguments - path to script and path to node.js
+
+const initialYargs = emptyYargs
+  .wrap(emptyYargs.terminalWidth())
   .env("KAI")
-  .example("kai export --apiKey=xxx --environmentId=xxx", "Creates zip backup of Kontent.ai project")
+  .example("$0 export --apiKey=xxx --environmentId=xxx", "Creates zip backup of Kontent.ai project")
   .example(
-    "kai import --apiKey=xxx --environmentId=xxx --fileName=backupFile",
+    "$0 import --apiKey=xxx --environmentId=xxx --fileName=backupFile",
     "Read given zip file and recreates data in Kontent.ai project",
   )
-  .demandCommand()
+  .epilogue("If you have any questions, contact us at devrel@kontent.ai.")
+  .demandCommand(1, chalk.red("You need to provide a command to run!"))
   .strict()
-  .help("h");
+  .help("h")
+  .alias("h", "help");
 
 commandsToRegister
   .reduce((currentYargs, registerCommand) => registerCommand(currentYargs), initialYargs)
