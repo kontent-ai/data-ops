328be14ca490394c45c41490ef7721eb665e0c64
Jiri Lojda <lojdaj@seznam.cz>
2024-01-18 17:21:27 +0100

    Add more import command integration tests

---
+++ tests/integration/importExport/utils/isEmpty.ts
@@ -0,0 +1,107 @@
+import { expect } from "@jest/globals";
+import { ManagementClient } from "@kontent-ai/management-sdk";
+
+const { API_KEY } = process.env;
+
+if (!API_KEY) {
+  throw new Error("API_KEY is missing in environment variables.");
+}
+
+export const expectNoPreviewUrls = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const previewUrls = await client
+    .getPreviewConfiguration()
+    .toPromise()
+    .then(res => res.data);
+
+  previewUrls.spaceDomains.forEach(d => expect(d.domain).toBe(""));
+  expect(previewUrls.previewUrlPatterns).toStrictEqual([]);
+};
+
+export const expectNoTaxonomies = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const taxonomies = await client
+    .listTaxonomies()
+    .toAllPromise()
+    .then(res => res.data.items);
+
+  expect(taxonomies).toStrictEqual([]);
+};
+
+export const expectNoAssetFolders = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const folders = await client
+    .listAssetFolders()
+    .toPromise()
+    .then(res => res.data.items);
+
+  expect(folders).toStrictEqual([]);
+};
+
+export const expectNoAssets = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const assets = await client
+    .listAssets()
+    .toAllPromise()
+    .then(res => res.data.items);
+
+  expect(assets).toStrictEqual([]);
+};
+
+export const expectNoWorkflows = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const [defaultWorkflow, ...restWorkflows] = await client
+    .listWorkflows()
+    .toPromise()
+    .then(res => res.data);
+
+  expect(restWorkflows).toStrictEqual([]);
+  expect(defaultWorkflow?.name).toBe("Default");
+  expect(defaultWorkflow?.codename).toBe("default");
+  expect(defaultWorkflow?.scopes).toStrictEqual([]);
+  expect(defaultWorkflow?.steps.map(s => s.codename)).toStrictEqual(["draft"]);
+};
+
+export const expectNoSnippets = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const assets = await client
+    .listContentTypeSnippets()
+    .toAllPromise()
+    .then(res => res.data.items);
+
+  expect(assets).toStrictEqual([]);
+};
+
+export const expectNoTypes = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const types = await client
+    .listContentTypes()
+    .toAllPromise()
+    .then(res => res.data.items);
+
+  expect(types).toStrictEqual([]);
+};
+
+export const expectNoItems = async (envId: string) => {
+  const client = makeClientFor(envId);
+
+  const items = await client
+    .listContentItems()
+    .toAllPromise()
+    .then(res => res.data.items);
+
+  expect(items).toStrictEqual([]);
+};
+
+const makeClientFor = (envId: string) =>
+  new ManagementClient({
+    apiKey: API_KEY,
+    environmentId: envId,
+  });
--- tests/integration/utils/runCommand.ts
+++ tests/integration/utils/runCommand.ts
@@ -1,7 +1,7 @@
 import * as childProcess from "child_process";
 
-export const runCommand = (command: string) => {
-  return new Promise((resolve, reject) => {
+export const runCommand = (command: string): Promise<CommandResult> => {
+  return new Promise((resolve, reject: (err: CommandError) => void) => {
     childProcess.exec(`node ./build/src/index.js ${command}`, (error, stdout, stderr) => {
       if (error) {
         reject({ error, stdout, stderr });
@@ -11,3 +11,14 @@ export const runCommand = (command: string) => {
     });
   });
 };
+
+export type CommandError = Readonly<{
+  error: childProcess.ExecException | null;
+  stdout: string;
+  stderr: string;
+}>;
+
+type CommandResult = Readonly<{
+  stdout: string;
+  stderr: string;
+}>;
