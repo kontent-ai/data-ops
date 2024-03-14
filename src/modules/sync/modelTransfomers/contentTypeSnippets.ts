a91f572feace2f7e223e11cf9c8a4e85fcee2bcc
Ivan Kiral <kiralivan@gmail.com>
2024-03-14 10:37:17 +0100

    add skeleton for transforming snippets

--- src/modules/sync/modelTransfomers/contentTypeSnippets.ts
+++ src/modules/sync/modelTransfomers/contentTypeSnippets.ts
@@ -1,11 +1,35 @@
+import { omit } from "../../../utils/object.js";
 import { EnvironmentModel } from "../generateSyncModel.js";
 import { ContentTypeSnippetsSyncModel } from "../types/fileContentModel.js";
 
 export const transformContentTypeSnippetsModel = (
-  environmentModel: EnvironmentModel,
+  environmentModel: EnvironmentModel
 ) => {
-  // TODO
-  environmentModel as never;
+  environmentModel.contentTypeSnippets.map(snippet => {
+    const syncSnippetElements: ContentTypeSnippetsSyncModel["elements"] = snippet.elements
+      .map(element => {
+        switch (element.type) {
+          case "guidelines":
+            return { ...element, codename: element.codename as string };
+          case "modular_content":
+            return { ...element, codename: element.codename as string };
+          case "taxonomy":
+            return { ...element, codename: element.codename as string };
+          case "multiple_choice":
+            return { ...element, codename: element.codename as string };
+          case "custom":
+            return { ...element, codename: element.codename as string };
+          case "asset":
+            return { ...element, codename: element.codename as string };
+          case "rich_text":
+            return { ...element, codename: element.codename as string };
+          default:
+            return { ...omit(element, ["id"]), codename: element.codename as string };
+        }
+      });
+
+    return { ...omit(snippet, ["id", "last_modified"]), elements: syncSnippetElements };
+  });
 
   return [] as ContentTypeSnippetsSyncModel[];
 };
