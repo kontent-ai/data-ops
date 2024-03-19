4ff8ce8e73dc8ef09f13c50e44c02f6e5ea2e2dc
Ivan Kiráľ <54802833+IvanKiral@users.noreply.github.com>
2024-03-19 11:56:23 +0100

    Add sync skeleton (#23)
    * Add sync skeleton
    
    * Make sync models immutable
    
    * refactor fixReferences name, naming for sync model
    
    * Update skeleton of patchOperations
    
    * fix type in filecontentModel
    
    * add fetching data from source environment with required assets and items
    
    * add test for contenTypeHelpers
    
    * rename modelGenerators folder to modelTransfomers
    
    * fix types for elements in sync models
    
    * pass whole environment model to transfomers
    
    * add type guards
    
    * fix getIds api, change traverse function to json transformer
    
    * update tests
    
    * remove diffconfig type
    
    * remove unnecesary generic from Set
    
    * return arrat instead of null
    
    * refactor
    
    ---------
    
    Co-authored-by: Jiri Lojda <lojdaj@seznam.cz>

---
+++ src/modules/sync/modelTransfomers/contentTypeSnippets.ts
@@ -0,0 +1,11 @@
+import { EnvironmentModel } from "../generateSyncModel.js";
+import { ContentTypeSnippetsSyncModel } from "../types/fileContentModel.js";
+
+export const transformContentTypeSnippetsModel = (
+  environmentModel: EnvironmentModel,
+) => {
+  // TODO
+  environmentModel as never;
+
+  return [] as ContentTypeSnippetsSyncModel[];
+};
