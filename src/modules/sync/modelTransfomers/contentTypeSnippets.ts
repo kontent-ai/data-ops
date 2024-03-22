91a58589dc641e8006e8e51c70752000b376cca1
Ivan Kiral <kiralivan@gmail.com>
2024-03-22 11:22:37 +0100

    add test for content type snippet transformer

--- src/modules/sync/modelTransfomers/contentTypeSnippets.ts
+++ src/modules/sync/modelTransfomers/contentTypeSnippets.ts
@@ -52,5 +52,5 @@ export const transformContentTypeSnippetsModel = (
         }
       });
 
-    return { ...omit(snippet, ["id", "last_modified"]), elements: syncSnippetElements };
+    return { ...omit(snippet, ["id", "last_modified"]), elements: syncSnippetElements, external_id: snippet.external_id ?? snippet.id };
   });
