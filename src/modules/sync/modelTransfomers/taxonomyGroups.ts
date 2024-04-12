9e7269f2dec48e38a84bb5c37fdc5ac6ea06dad9
Ivan Kiral <kiralivan@gmail.com>
2024-04-12 15:54:44 +0200

    update external_ids to use codenames instead ids

--- src/modules/sync/modelTransfomers/taxonomyGroups.ts
+++ src/modules/sync/modelTransfomers/taxonomyGroups.ts
@@ -5,11 +5,12 @@ import { TaxonomySyncModel } from "../types/fileContentModel.js";
 
 export const transformTaxonomyGroupsModel = (
   taxonomies: ReadonlyArray<TaxonomyContracts.ITaxonomyContract>,
+  taxonomyGroupCodename: string | undefined = undefined,
 ): ReadonlyArray<TaxonomySyncModel> =>
   taxonomies.map(t =>
     extractNulls({
       ...omit(t, ["id", "last_modified"]),
-      terms: transformTaxonomyGroupsModel(t.terms),
-      external_id: t.external_id ?? t.id,
+      terms: transformTaxonomyGroupsModel(t.terms, taxonomyGroupCodename ?? t.codename),
+      external_id: t.external_id ?? `${taxonomyGroupCodename ? taxonomyGroupCodename + "-" : ""}${t.codename}`,
     }) as TaxonomySyncModel
   );
