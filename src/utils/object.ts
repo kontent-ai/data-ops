03627c21078434fe02b2007d207ad1f39600dd79
Ivan Kiral <kiralivan@gmail.com>
2024-04-15 13:11:04 +0200

    Refactor extractNulls name and add items from subpages element

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -3,17 +3,17 @@ import { SuperiorOmit } from "./types.js";
 export const omit = <T extends object, K extends keyof T>(obj: T, props: K[]): SuperiorOmit<T, K> =>
   Object.fromEntries(Object.entries(obj).filter(([key]) => !props.includes(key as K))) as SuperiorOmit<T, K>;
 
-export const extractNulls = (value: unknown): unknown => {
+export const removeNulls = (value: unknown): unknown => {
   if (value === null) {
     return undefined;
   }
 
   if (Array.isArray(value)) {
-    return value.map(extractNulls);
+    return value.map(removeNulls);
   }
 
   if (typeof value === "object") {
-    return Object.fromEntries(extractNulls(Object.entries(value)) as ReadonlyArray<[string, unknown]>);
+    return Object.fromEntries(removeNulls(Object.entries(value)) as ReadonlyArray<[string, unknown]>);
   }
 
   return value;
