db622dbae6a64003cb3493f3db17eda1377d144c
Ivan Kiral <kiralivan@gmail.com>
2024-04-15 13:11:04 +0200

    Refactor extractNulls name and add items from subpages element

--- src/modules/sync/modelTransfomers/taxonomyGroups.ts
+++ src/modules/sync/modelTransfomers/taxonomyGroups.ts
@@ -1,6 +1,6 @@
 import { TaxonomyContracts } from "@kontent-ai/management-sdk";
 
-import { extractNulls, omit } from "../../../utils/object.js";
+import { omit, removeNulls } from "../../../utils/object.js";
 import { TaxonomySyncModel } from "../types/fileContentModel.js";
 
 export const transformTaxonomyGroupsModel = (
@@ -8,7 +8,7 @@ export const transformTaxonomyGroupsModel = (
   taxonomyGroupCodename: string | undefined = undefined,
 ): ReadonlyArray<TaxonomySyncModel> =>
   taxonomies.map(t =>
-    extractNulls({
+    removeNulls({
       ...omit(t, ["id", "last_modified"]),
       terms: transformTaxonomyGroupsModel(t.terms, taxonomyGroupCodename ?? t.codename),
       external_id: t.external_id ?? `${taxonomyGroupCodename ? taxonomyGroupCodename + "-" : ""}${t.codename}`,
