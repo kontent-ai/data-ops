1b4c57f471544b8f89be4698841e46730a180714
Daniel Pokorny <danielp@kontent.ai>
2024-04-17 13:17:25 +0200

    refactor error handling

--- src/utils/typeguards.ts
+++ src/utils/typeguards.ts
@@ -1,3 +1,8 @@
+import { SharedModels } from "@kontent-ai/management-sdk";
+import { spotlightInUseErrorCode } from "../constants/responseCodes.js";
 export const notNull = <T>(arg: T | null): arg is T => arg !== null;
 
 export const notNullOrUndefined = <T>(arg: T | undefined | null): arg is T => arg !== undefined && arg !== null;
+
+export const isSpotlightInUseError = (err: any): err is SharedModels.ContentManagementBaseKontentError =>
+  err.errorCode === spotlightInUseErrorCode;
