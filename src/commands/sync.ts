ffb0a66181edf0ada9969b5553efe54734fc58b3
Ivan Kiral <kiralivan@gmail.com>
2024-04-25 13:26:31 +0200

    update console interface to ask to continue after print

--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -1,6 +1,14 @@
+import chalk from "chalk";
+
 import { logError, LogOptions } from "../log.js";
+import { diff } from "../modules/sync/diff.js";
+import { fetchModel, transformSyncModel } from "../modules/sync/generateSyncModel.js";
+import { printDiff } from "../modules/sync/printDiff.js";
+import { requestConfirmation } from "../modules/sync/utils/consoleHelpers.js";
+import { readContentModelFromFolder } from "../modules/sync/utils/getContentModel.js";
 import { validateContentFolder } from "../modules/sync/validation.js";
 import { RegisterCommand } from "../types/yargs.js";
+import { throwError } from "../utils/error.js";
 
 export const register: RegisterCommand = yargs =>
   yargs.command({
@@ -37,6 +45,11 @@ export const register: RegisterCommand = yargs =>
           describe: "Management API key of Kontent.ai environmnent containing source content model",
           conflicts: "folderName",
           implies: ["sourceEnvironmentId"],
+        })
+        .option("skipWarning", {
+          type: "boolean",
+          describe: "Skip warning message.",
+          alias: "s",
         }),
     handler: args => syncContentModel(args),
   });
@@ -48,6 +61,7 @@ export type SyncParams =
     folderName?: string;
     sourceEnvironmentId?: string;
     sourceApiKey?: string;
+    skipWarning?: boolean;
   }>
   & LogOptions;
 
@@ -58,6 +72,41 @@ export const syncContentModel = async (params: SyncParams) => {
       folderErrors.forEach(e => logError(params, "standard", e));
       process.exit(1);
     }
+  }
+
+  const sourceModel = params.folderName
+    ? await readContentModelFromFolder(params.folderName)
+    : transformSyncModel(
+      await fetchModel({
+        environmentId: params.sourceEnvironmentId ?? throwError("sourceEnvironmentId should not be undefined"),
+        apiKey: params.sourceApiKey ?? throwError("sourceApiKey should not be undefined"),
+      }),
+      params,
+    );
+
+  const targetModel = await fetchModel({ apiKey: params.apiKey, environmentId: params.environmentId });
+  const assetsReferences = new Map(targetModel.assets.map(a => [a.codename, { id: a.id, codename: a.codename }]));
+  const itemReferences = new Map(targetModel.items.map(i => [i.codename, { id: i.id, codename: i.codename }]));
+  const transformedTargetModel = transformSyncModel(targetModel, params);
+
+  const diffModel = diff({
+    targetAssetsReferencedFromSourceByCodenames: assetsReferences,
+    targetItemsReferencedFromSourceByCodenames: itemReferences,
+    targetEnvModel: transformedTargetModel,
+    sourceEnvModel: sourceModel,
+  });
+
+  printDiff(diffModel, params);
+
+  const warningMessage = chalk.yellow(
+    `⚠ Running this operation may result in irreversible changes to the content in environment ${params.environmentId}.\n\nOK to proceed y/n? (suppress this message with -s parameter)\n`,
+  );
+
+  const confirmed = !params.skipWarning ? await requestConfirmation(warningMessage) : true;
+
+  if (!confirmed) {
+    logError(params, chalk.red("Operation aborted."));
+    process.exit(1);
   }
 
   // uncomment when dealing with sync
