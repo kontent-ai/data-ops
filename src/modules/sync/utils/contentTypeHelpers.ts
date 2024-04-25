7413c048b3be21dd55b20d4acf4ea77acb01fb43
Ivan Kiral <kiralivan@gmail.com>
2024-04-25 16:02:16 +0200

    add getRequiredCodenames function

--- src/modules/sync/utils/contentTypeHelpers.ts
+++ src/modules/sync/utils/contentTypeHelpers.ts
@@ -9,6 +9,10 @@ import {
   transformToJson,
 } from "@kontent-ai/rich-text-resolver";
 
+import { notNullOrUndefined } from "../../../utils/typeguards.js";
+import { getAssetReferences, getItemReferences, OriginalReference } from "../diff/guidelinesRichText.js";
+import { SyncAssetElement, SyncLinkedItemsElement, SyncTypeElement } from "../types/syncModel.js";
+
 const resolveAssetIdsDomHtmlNode: ResolveDomHtmlNodeType = (node, traverse) => {
   switch (node.tagName) {
     case "figure":
@@ -78,6 +82,43 @@ const getRequiredItemOrAssetIds = (
         resolveDomHtmlNode: resolveDomTextNode,
       }),
     ] as string[], []).flat(Infinity).filter(g => g);
+
+  return new Set([...elementsIds, ...idsFromGuidelines]);
+};
+
+export const getRequiredCodenames = (
+  elements: ReadonlyArray<SyncTypeElement>,
+) => {
+  const assetElements = elements.filter((element): element is SyncAssetElement => element.type === "asset");
+  // Use typeguard once types in SDKs are fixed
+  const guidelinesElements = elements.filter(element =>
+    element.type === "guidelines"
+  ) as unknown as ContentTypeElements.IGuidelinesElement[];
+  const linkedItemElements = elements.filter((element): element is SyncLinkedItemsElement =>
+    // currently, the subpages type in SDK does not contain default property, therefore subpages are narrowed to ILinkedItemsElement.
+    // since the subpages element is the same as the linked items element apart from the type property.
+    element.type === "modular_content" || element.type === "subpages"
+  );
+
+  const guidelines = guidelinesElements.map(g => g.guidelines);
+
+  const assetCodenames = getRequiredItemOrAssetCodenames(assetElements, getAssetReferences, guidelines);
+  const itemCodenames = getRequiredItemOrAssetCodenames(linkedItemElements, getItemReferences, guidelines);
+
+  return { assetCodenames, itemCodenames };
+};
+
+const getRequiredItemOrAssetCodenames = (
+  elements: { readonly default?: { readonly global: { readonly value: ReadonlyArray<{ codename: string }> } } }[],
+  getFromGuidelines: (guidelines: string) => readonly OriginalReference[],
+  guidelines: string[],
+) => {
+  const elementsIds = new Set(
+    elements.flatMap(e => e.default?.global.value.map(r => r.codename)).filter(notNullOrUndefined),
+  );
+
+  const idsFromGuidelines = guidelines.flatMap(guideline => getFromGuidelines(guideline).flatMap(g => g.codename))
+    .filter(notNullOrUndefined);
 
   return new Set([...elementsIds, ...idsFromGuidelines]);
 };
