7aba42e8176965c478df41c1b4a89cdd22e88281
Ivan Kiral <kiralivan@gmail.com>
2024-05-21 15:02:33 +0200

    add validaiton to diffed model

--- src/modules/sync/validation.ts
+++ src/modules/sync/validation.ts
@@ -1,8 +1,14 @@
+import { ManagementClient } from "@kontent-ai/management-sdk";
 import chalk from "chalk";
 import * as fs from "fs/promises";
 import * as path from "path";
 
+import { serially } from "../../utils/requests.js";
+import { notNullOrUndefined } from "../../utils/typeguards.js";
+import { elementTypes } from "./constants/elements.js";
 import { contentTypesFileName, contentTypeSnippetsFileName, taxonomiesFileName } from "./constants/filename.js";
+import { ElementsTypes } from "./types/contractModels.js";
+import { DiffModel, PatchOperation } from "./types/diffModel.js";
 import { FileContentModel } from "./types/fileContentModel.js";
 import { extractTerms } from "./utils/taxonomyGroupHelpers.js";
 
@@ -40,6 +46,51 @@ export const validateContentModel = async (
   ];
 };
 
+export const validateDiffedModel = async (
+  client: ManagementClient,
+  diffedModel: DiffModel,
+) => {
+  const typeUsage = await getUsedContentTypesCodenames(client, diffedModel.contentTypes.deleted);
+
+  const typeUsageErrors = typeUsage.map((typeCodename) =>
+    `Content type (codename: ${typeCodename}) is being used and can't be removed (In given environment exists content items of given content type)`
+  );
+
+  const changeElementTypeOps = getElementChangeTypeOps([
+    ...diffedModel.contentTypes.updated,
+    ...diffedModel.contentTypeSnippets.updated,
+  ]);
+
+  const changeElementOpsErrors = changeElementTypeOps.flatMap(([entityCodename, ops]) =>
+    ops.map(op =>
+      `Detected unsupported operation: changing element's (codename: ${op.oldValue.codename}) 'type'(from: ${op.oldValue.type} to: ${op.value.type}) property in entity (codename: ${entityCodename})`
+    )
+  );
+
+  return [...typeUsageErrors, ...changeElementOpsErrors];
+};
+
+type ElementTypeCodename = Readonly<{ type: ElementsTypes; codename: string }>;
+type ReplaceElementOperation = Readonly<
+  Omit<Extract<PatchOperation, { op: "replace" }>, "oldValue" | "value"> & {
+    oldValue: ElementTypeCodename;
+    value: ElementTypeCodename;
+  }
+>;
+
+const isElement = (el: unknown): el is ElementTypeCodename =>
+  typeof el === "object"
+  && el !== null
+  && "type" in el
+  && typeof el.type === "string"
+  && "codename" in el
+  && typeof (el.codename) === "string"
+  && elementTypes.has(el.type);
+
+const isReplaceElementOp = (
+  op: PatchOperation,
+): op is ReplaceElementOperation => op.op === "replace" && isElement(op.oldValue) && isElement(op.value);
+
 type EntityBase = Readonly<{ codename: string; external_id?: string }>;
 
 const handleDiffObjectsSameExtId = (
@@ -63,3 +114,29 @@ const handleDiffObjectsSameExtId = (
         ]
         : [];
     });
+
+const getUsedContentTypesCodenames = async (client: ManagementClient, contentTypeCodenames: ReadonlySet<string>) => {
+  const promises = [...contentTypeCodenames].map(typeCodename => () =>
+    client
+      .listLanguageVariantsOfContentType()
+      .byTypeCodename(typeCodename)
+      .toPromise()
+      .then(res => res.data.items.length ? typeCodename : null)
+  );
+
+  return (await serially(promises))
+    .filter(notNullOrUndefined);
+};
+
+const getElementChangeTypeOps = (
+  updateOps: ReadonlyArray<[string, ReadonlyArray<PatchOperation>]>,
+) =>
+  updateOps
+    .map(([entityCodename, ops]) =>
+      [
+        entityCodename,
+        ops.filter(isReplaceElementOp)
+          .filter(op => op.oldValue.type !== op.value.type),
+      ] as const
+    )
+    .filter(([, ops]) => ops.length);
