5b0f39def6d38227fa0c614510cf7873fa3ebfa1
Ivan Kiral <kiralivan@gmail.com>
2024-05-21 14:51:59 +0200

    Fix order of operations to execute replace addinto snippets operations before content types

---
+++ src/modules/sync/constants/elements.ts
@@ -0,0 +1,19 @@
+import { ElementsTypes } from "../types/contractModels.js";
+
+const elementTypesObj: { [key in ElementsTypes]: null } = {
+  asset: null,
+  number: null,
+  snippet: null,
+  custom: null,
+  date_time: null,
+  modular_content: null,
+  guidelines: null,
+  subpages: null,
+  multiple_choice: null,
+  rich_text: null,
+  taxonomy: null,
+  text: null,
+  url_slug: null,
+};
+
+export const elementTypes = new Set(Object.keys(elementTypesObj));
