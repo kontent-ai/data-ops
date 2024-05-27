8a75265199733107efc0577166c569ad09526bfa
Jiri Lojda <lojdaj@seznam.cz>
2024-05-27 15:18:55 +0200

    Add retry for 5xx responses

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,5 +1,6 @@
-import { HttpService } from "@kontent-ai/core-sdk";
+import { HttpService, IHttpService, IRetryStrategyOptions, retryHelper } from "@kontent-ai/core-sdk";
 import { ManagementClient } from "@kontent-ai/management-sdk";
+import { CancelToken, isAxiosError } from "axios";
 
 import packageJson from "../../package.json" with { type: "json" };
 
@@ -15,10 +16,30 @@ export const createClient = ({ environmentId, apiKey, commandName }: Params): Ma
   new ManagementClient({
     environmentId,
     apiKey,
-    httpService: new HttpService({
-      logErrorsToConsole: false,
-      axiosRequestConfig: {
-        headers: { [sourceTrackingHeaderName]: `${packageJson.name};${packageJson.version};${commandName}` },
-      },
-    }),
+    httpService: createHttpService(commandName),
   });
+
+const createHttpService = (commandName: string): IHttpService<CancelToken> => {
+  const originalHttpService = new HttpService({
+    logErrorsToConsole: false,
+    axiosRequestConfig: {
+      headers: { [sourceTrackingHeaderName]: `${packageJson.name};${packageJson.version};${commandName}` },
+    },
+  });
+
+  const retryStrategy: IRetryStrategyOptions = {
+    ...retryHelper.defaultRetryStrategy,
+    canRetryError: (error: any) =>
+      retryHelper.defaultRetryStrategy.canRetryError(error)
+      || (isAxiosError(error) && !!error.response?.status.toString().startsWith("5")),
+  };
+
+  return {
+    getAsync: (call, config) => originalHttpService.getAsync(call, { ...config, retryStrategy }),
+    postAsync: (call, config) => originalHttpService.postAsync(call, { ...config, retryStrategy }),
+    putAsync: (call, config) => originalHttpService.putAsync(call, { ...config, retryStrategy }),
+    patchAsync: (call, config) => originalHttpService.patchAsync(call, { ...config, retryStrategy }),
+    deleteAsync: (call, config) => originalHttpService.deleteAsync(call, { ...config, retryStrategy }),
+    createCancelToken: originalHttpService.createCancelToken,
+  };
+};
