5468edec1f7e36fb4077ece2b0508272116ef018
Ivan Kiral <kiralivan@gmail.com>
2024-05-28 15:41:07 +0200

    add sync integration tests

---
+++ tests/integration/sync/syncEnvironments.test.ts
@@ -0,0 +1,95 @@
+import { describe, expect, it } from "@jest/globals";
+import * as fsPromises from "fs/promises";
+import * as path from "path";
+
+import {
+  contentTypesFileName,
+  contentTypeSnippetsFileName,
+  taxonomiesFileName,
+} from "../../../src/modules/sync/constants/filename";
+import { expectSameEnvironments } from "../importExport/utils/compare";
+import { runCommand } from "../utils/runCommand";
+import { withTestEnvironment } from "../utils/setup";
+
+const { SYNC_SOURCE_TEST_ENVIRONMENT_ID, API_KEY, SYNC_TARGET_TEST_ENVIRONMENT_ID } = process.env;
+
+if (!SYNC_SOURCE_TEST_ENVIRONMENT_ID) {
+  throw new Error("SYNC_SOURCE_TEST_ENVIRONMENT_ID environment variable is not defined.");
+}
+if (!SYNC_TARGET_TEST_ENVIRONMENT_ID) {
+  throw new Error("SYNC_TARGET_TEST_ENVIRONMENT_ID env variable was not provided.");
+}
+if (!API_KEY) {
+  throw new Error("API_KEY env variable was not provided.");
+}
+
+describe("Sync two environments with credentials", () => {
+  it.concurrent(
+    "Sync source environment to target environment with providing credentials for both",
+    withTestEnvironment(SYNC_TARGET_TEST_ENVIRONMENT_ID, async (environmentId) => {
+      const command =
+        `sync -s=${SYNC_SOURCE_TEST_ENVIRONMENT_ID} --sk=${API_KEY} -t=${environmentId} --tk=${API_KEY} --verbose --skipConfirmation`;
+
+      await runCommand(command);
+
+      await expectSameEnvironments(environmentId, SYNC_SOURCE_TEST_ENVIRONMENT_ID, {
+        include: ["types", "taxonomies", "snippets"],
+      });
+    }),
+  );
+
+  it.concurrent(
+    "Sync target environment to source environment with providing credentials for both",
+    withTestEnvironment(SYNC_SOURCE_TEST_ENVIRONMENT_ID, async (environmentId) => {
+      const command =
+        `sync -s=${SYNC_TARGET_TEST_ENVIRONMENT_ID} --sk=${API_KEY} -t=${environmentId} --tk=${API_KEY} --verbose --skipConfirmation`;
+
+      await runCommand(command);
+
+      await expectSameEnvironments(environmentId, SYNC_TARGET_TEST_ENVIRONMENT_ID, {
+        include: ["types", "taxonomies", "snippets"],
+      });
+    }),
+  );
+});
+
+describe("Sync environment from folder", () => {
+  const folderPath = path.join(__dirname, "data/sourceContentModel");
+
+  it.only("generate sync model test", async () => {
+    const command = `generate-sync-model -e ${SYNC_SOURCE_TEST_ENVIRONMENT_ID} -k ${API_KEY} -f ${folderPath}`;
+    await runCommand(command);
+
+    const folderExists = await fsPromises.stat(folderPath)
+      .then(stats => stats.isDirectory())
+      .catch(() => false);
+
+    const typesJsonExists = await fsPromises.stat(`${folderPath}/${contentTypesFileName}`)
+      .then(stats => stats.isFile())
+      .catch(() => false);
+    const snippetJsonExists = await fsPromises.stat(`${folderPath}/${contentTypeSnippetsFileName}`)
+      .then(stats => stats.isFile())
+      .catch(() => false);
+    const taxonomiesJsonExists = await fsPromises.stat(`${folderPath}/${taxonomiesFileName}`)
+      .then(stats => stats.isFile())
+      .catch(() => false);
+
+    expect(folderExists).toEqual(true);
+    expect(typesJsonExists).toEqual(true);
+    expect(snippetJsonExists).toEqual(true);
+    expect(taxonomiesJsonExists).toEqual(true);
+  });
+
+  it(
+    "Sync environment from folder",
+    withTestEnvironment(SYNC_TARGET_TEST_ENVIRONMENT_ID, async (environmentId) => {
+      const command = `sync -t=${environmentId} --tk=${API_KEY} -f=${folderPath} --verbose --skipConfirmation`;
+
+      await runCommand(command);
+
+      await expectSameEnvironments(environmentId, SYNC_SOURCE_TEST_ENVIRONMENT_ID, {
+        include: ["types", "taxonomies", "snippets"],
+      });
+    }),
+  );
+});
