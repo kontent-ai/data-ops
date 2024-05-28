ad484a06f34bdccac0216f2c24d75cd35fa4313d
Daniel Pokorny <danielp@kontent.ai>
2024-05-28 10:02:23 +0200

    refactor templating

--- src/modules/sync/advancedDiff.ts
+++ src/modules/sync/advancedDiff.ts
@@ -11,11 +11,11 @@ import { DiffModel, DiffObject, PatchOperation } from "./types/diffModel.js";
 
 type DiffData = DiffModel & SyncParams;
 
-export const resolveTemplate = (diffData: DiffData) => {
+export const writeDiffToFile = (diffData: DiffData) => {
   const template = readFileSync(resolve("./", "diffTemplate.html"), "utf-8");
   const resolvedTemplate = template.replace(
     /{{.*?}}/g,
-    (match) => templateMap.get(match)?.(diffData) ?? match,
+    (match) => templateHandlerMap.get(match)?.(diffData) ?? match,
   );
   const outputPath = resolve(
     "./",
@@ -30,7 +30,7 @@ export const resolveTemplate = (diffData: DiffData) => {
     );
     writeFileSync(outputPath, resolvedTemplate);
   } catch (err) {
-    logError(diffData, "Failed writing a diff file: ", JSON.stringify(err));
+    logError(diffData, `Failed writing a diff file: ${JSON.stringify(err)}`);
     process.exit(1);
   }
 
@@ -42,9 +42,7 @@ export const resolveTemplate = (diffData: DiffData) => {
   open(outputPath).catch((err) =>
     logError(
       diffData,
-      chalk.red("Failed to open the file: "),
-      outputPath,
-      JSON.stringify(err),
+      `Failed to open the file: ${outputPath}\nMessage: ${JSON.stringify(err)}`
     )
   );
 };
@@ -71,7 +69,7 @@ const processPatchOp = (patchOp: PatchOperation) => {
       }`;
     case "addInto":
       return `Path ${patchOp.path} with ${
-        extractEntityIdentifier(
+        getValueOrIdentifier(
           patchOp.value,
         )
       } to be added ${patchOp.before ? `before ${patchOp.before.codename}` : ""} ${
@@ -79,28 +77,42 @@ const processPatchOp = (patchOp: PatchOperation) => {
       }`;
     case "replace":
       return `Path ${patchOp.path} value replaced.\n - From: "${
-        extractEntityIdentifier(
+        getValueOrIdentifier(
           patchOp.oldValue,
         )
-      }" \n - To: ${extractEntityIdentifier(patchOp.value)}`;
+      }" \n - To: ${getValueOrIdentifier(patchOp.value)}`;
   }
 };
 
-const processAddedEntities = <T>(entity: T): string => {
+const processAddedEntities = (entity: unknown): string => {
   if (isTaxonomyRequestModel(entity)) {
-    return `<pre><h2>${entity.name}</h2><h4>${entity.terms.map(e => e.name).join(", ")}</h4></pre>`;
-  } else if (isContentTypeData(entity)) {
-    return `<pre><h2>${entity.name}</h2><h4>${
-      entity.elements.flatMap(e => `${e.codename}: ${e.type}`).join(", ")
-    }</h4></pre>`;
-  } else if (isContentTypeSnippetData(entity)) {
-    return `<pre><h2>${entity.name}</h2><h4>${
-      entity.elements.flatMap(e => `${e.codename}: ${e.type}`).join(", ")
-    }</h4></pre>`;
-  } else {
-    return `Unknown type`;
+    return `<pre><h2>${entity.name}</h2><h4>${entity.terms
+      .map((e) => e.name)
+      .join(", ")}</h4></pre>`;
   }
+
+  if (isContentTypeData(entity)) {
+    return `<pre><h2>${entity.name}</h2><h4>${entity.elements
+      .flatMap((e) => `${e.codename}: ${e.type}`)
+      .join(", ")}</h4></pre>`;
+  }
+  if (isContentTypeSnippetData(entity)) {
+    return `<pre><h2>${entity.name}</h2><h4>${entity.elements
+      .flatMap((e) => `${e.codename}: ${e.type}`)
+      .join(", ")}</h4></pre>`;
+  }
+  return `Unknown type`;
 };
+
+const getValueOrIdentifier = (value: unknown): string | string[] =>
+  Array.isArray(value)
+    ? value.flatMap(getValueOrIdentifier)
+    : typeof value === "object"
+        && value
+        && (("codename" in value && typeof value.codename === "string")
+          || ("id" in value && typeof value.id === "string"))
+    ? `"${"codename" in value ? value.codename : value.id}"`
+    : `${value}`;
 
 const createEntitiesToAdd = <T extends { codename: string }>(
   diff: Pick<DiffObject<RequiredCodename<T>>, "added">,
@@ -115,102 +127,101 @@ const createEntitiesToUpdate = <T extends { codename: string }>(
 ) =>
   [...diff.updated]
     .flatMap(
-      ([k, v]) =>
-        `<pre><h2>${k}</h2><pre>${
-          v
+      ([entity, patchOps]) =>
+        `<pre><h2>${entity}</h2><pre>${
+          patchOps
             .flatMap(processPatchOp)
             .join("\n")
         }</pre></pre>`,
     )
     .join("\n");
 
-const templateMap: Map<string, (diff: DiffData) => string> = new Map([
+const templateHandlerMap: Map<string, (diff: DiffData) => string> = new Map([
   [
     "{{added_content_types}}",
-    (diffData: DiffData) => createEntitiesToAdd(diffData.contentTypes),
+    ({ contentTypes }: DiffData) => createEntitiesToAdd(contentTypes),
   ],
   [
     "{{removed_content_types}}",
-    (diffData: DiffData) => createEntitiesToDelete(diffData.contentTypes),
+    ({ contentTypes }: DiffData) => createEntitiesToDelete(contentTypes),
   ],
   [
     "{{modified_content_types}}",
-    (diffData: DiffData) => createEntitiesToUpdate(diffData.contentTypes),
+    ({ contentTypes }: DiffData) => createEntitiesToUpdate(contentTypes),
   ],
   [
     "{{added_content_type_snippets}}",
-    (diffData: DiffData) => createEntitiesToAdd(diffData.contentTypeSnippets),
+    ({ contentTypeSnippets }: DiffData) => createEntitiesToAdd(contentTypeSnippets),
   ],
   [
     "{{removed_content_type_snippets}}",
-    (diffData: DiffData) => createEntitiesToDelete(diffData.contentTypeSnippets),
+    ({ contentTypeSnippets }: DiffData) => createEntitiesToDelete(contentTypeSnippets),
   ],
   [
     "{{modified_content_type_snippets}}",
-    (diffData: DiffData) => createEntitiesToUpdate(diffData.contentTypeSnippets),
+    ({ contentTypeSnippets }: DiffData) => createEntitiesToUpdate(contentTypeSnippets),
   ],
   [
     "{{added_taxonomy_groups}}",
-    (diffData: DiffData) => createEntitiesToAdd(diffData.taxonomyGroups),
+    ({ taxonomyGroups }: DiffData) => createEntitiesToAdd(taxonomyGroups),
   ],
   [
     "{{removed_taxonomy_groups}}",
-    (diffData: DiffData) => createEntitiesToDelete(diffData.taxonomyGroups),
+    ({ taxonomyGroups }: DiffData) => createEntitiesToDelete(taxonomyGroups),
   ],
   [
     "{{modified_taxonomy_groups}}",
-    (diffData: DiffData) => createEntitiesToUpdate(diffData.taxonomyGroups),
+    ({ taxonomyGroups }: DiffData) => createEntitiesToUpdate(taxonomyGroups),
   ],
   [
     "{{num_types_to_add}}",
-    (diffData: DiffData) => diffData.contentTypes.added.length.toString(),
+    ({ contentTypes }: DiffData) => contentTypes.added.length.toString(),
   ],
   [
     "{{num_types_to_remove}}",
-    (diffData: DiffData) => diffData.contentTypes.deleted.size.toString(),
+    ({ contentTypes }: DiffData) => contentTypes.deleted.size.toString(),
   ],
   [
     "{{num_types_to_mod}}",
-    (diffData: DiffData) => diffData.contentTypes.updated.size.toString(),
+    ({ contentTypes }: DiffData) => contentTypes.updated.size.toString(),
   ],
   [
     "{{num_snippets_to_add}}",
-    (diffData: DiffData) => diffData.contentTypeSnippets.added.length.toString(),
+    ({ contentTypeSnippets }: DiffData) => contentTypeSnippets.added.length.toString(),
   ],
   [
     "{{num_snippets_to_remove}}",
-    (diffData: DiffData) => diffData.contentTypeSnippets.deleted.size.toString(),
+    ({ contentTypeSnippets }: DiffData) => contentTypeSnippets.deleted.size.toString(),
   ],
   [
     "{{num_snippets_to_mod}}",
-    (diffData: DiffData) => diffData.contentTypeSnippets.updated.size.toString(),
+    ({ contentTypeSnippets }: DiffData) => contentTypeSnippets.updated.size.toString(),
   ],
   [
     "{{num_taxonomy_to_add}}",
-    (diffData: DiffData) => diffData.taxonomyGroups.added.length.toString(),
+    ({ taxonomyGroups }: DiffData) => taxonomyGroups.added.length.toString(),
   ],
   [
     "{{num_taxonomy_to_remove}}",
-    (diffData: DiffData) => diffData.taxonomyGroups.deleted.size.toString(),
+    ({ taxonomyGroups }: DiffData) => taxonomyGroups.deleted.size.toString(),
   ],
   [
     "{{num_taxonomy_to_mod}}",
-    (diffData: DiffData) => diffData.taxonomyGroups.updated.size.toString(),
+    ({ taxonomyGroups }: DiffData) => taxonomyGroups.updated.size.toString(),
   ],
   [
     "{{total_changes}}",
-    (diffModel: DiffModel) =>
-      Object.values(diffModel)
-        .filter(d => "added" in d)
-        .reduce<number>(
-          (acc, curr) => acc + curr.added.length + curr.deleted.size + curr.updated.size,
+    ({ taxonomyGroups, contentTypes, contentTypeSnippets }: DiffData) =>
+      [taxonomyGroups, contentTypes, contentTypeSnippets]
+        .reduce(
+          (acc, {added, deleted, updated}) => acc + added.length + deleted.size + updated.size,
           0,
         )
         .toString(),
   ],
   [
     "{{source_env_id}}",
-    (syncParams: SyncParams) => syncParams.sourceEnvironmentId ?? "N/A (Sourced from a folder)",
+    ({ sourceEnvironmentId }: DiffData) => sourceEnvironmentId ?? "N/A (Sourced from a folder)",
   ],
-  ["{{target_env_id}}", (syncParams: SyncParams) => syncParams.environmentId],
+  ["{{target_env_id}}", ({ environmentId }: DiffData) => environmentId],
 ]);
