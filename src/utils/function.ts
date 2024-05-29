575413b166571c9a03aca99f43d95f8e4b5587e1
Jiri Lojda <lojdaj@seznam.cz>
2024-05-29 13:11:18 +0200

    Set mode to single in multi-choice last to avoid conflicts with default value

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -4,3 +4,5 @@ export const apply = <Input, Output>(
   fnc: (v: Input) => Output,
   value: Input | null | undefined,
 ): Output | null | undefined => notNullOrUndefined(value) ? fnc(value) : value;
+
+export const not = <T>(fnc: (a: T) => boolean) => (value: T): boolean => !fnc(value);
