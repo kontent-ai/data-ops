6bb2265f78c84e1a660c26df7689f3498f0ade71
Ivan Kiral <kiralivan@gmail.com>
2024-06-18 13:27:27 +0200

    update add api

--- src/commands/migrations/add/add.ts
+++ src/commands/migrations/add/add.ts
@@ -12,24 +12,34 @@ export const register: RegisterCommand = yargs =>
         .option("name", {
           alias: "n",
           describe: "Name of the migration",
-          demandOption: "You need to provide the name for the migration.",
+          demandOption: "You need to provide name of the migration",
+          type: "string",
         })
         .option("folder", {
           alias: "f",
+          type: "string",
           describe: "Path to folder where should migration be stored.",
         })
         .option("type", {
           alias: "t",
           describe: "Type of the scirpt. Allowed values 'ts' or 'js'. Default ts.",
+          type: "string",
+          default: "js",
           choices: ["js", "ts"],
+        })
+        .option("timestamp", {
+          alias: "d",
+          describe: "Adds the the current DateTime as order. Also the migration name starts with date.",
+          type: "boolean",
+          default: false,
         }),
-    handler: () => {},
   });
 
 export type AddMigrationParams =
   & Readonly<{
     name: string;
     folder?: string;
-    type?: "js" | "ts";
+    timestamp: boolean;
+    type: string;
   }>
   & LogOptions;
