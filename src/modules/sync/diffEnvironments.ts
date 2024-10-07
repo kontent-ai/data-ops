1982b684b16d7bc586785e998ed20cb334ef0698
Ivan Kiral <kiralivan@gmail.com>
2024-10-07 13:13:41 +0200

    update diffEnvironments to return html

--- src/modules/sync/diffEnvironments.ts
+++ src/modules/sync/diffEnvironments.ts
@@ -1,16 +1,17 @@
 import chalk from "chalk";
+import { resolve } from "path";
 
 import { logError, logInfo, LogOptions } from "../../log.js";
 import { createClient } from "../../utils/client.js";
-import { syncEntityChoices } from "./constants/entities.js";
 import { diff } from "./diff.js";
 import { fetchModel, transformSyncModel } from "./generateSyncModel.js";
-import { createAdvancedDiffFile, printDiff } from "./printDiff.js";
+import { readHtmlFile } from "./utils/fileUtils.js";
 import {
   getSourceItemAndAssetCodenames,
   getTargetContentModel,
   readContentModelFromFolder,
 } from "./utils/getContentModel.js";
+import { resolveHtmlTemplate } from "./utils/htmlRenderers.js";
 
 export type DiffEnvironmentsParams = Readonly<
   & {
@@ -21,18 +22,21 @@ export type DiffEnvironmentsParams = Readonly<
     | { folderName: string }
     | { folderName?: undefined; sourceEnvironmentId: string; sourceApiKey: string }
   )
-  & (
-    {
-      advanced: true;
-      outPath?: string;
-      noOpen?: boolean;
-    } | { advanced?: false }
-  )
   & LogOptions
 >;
 
+/**
+ * Compares two environments and generates an HTML representation of the differences.
+ * The output includes differences using patch operations for better clarity of what has
+ * been added, removed, or modified between the two environments.
+ *
+ * @returns {Promise<string>} HTML string containing the visual representation of the diff between the two environments.
+ */
 export const diffEnvironments = async (params: DiffEnvironmentsParams) => {
-  await diffEnvironmentsInternal(params, "diff-API");
+  const diffModel = await diffEnvironmentsInternal(params, "diff-API");
+  const templateString = readHtmlFile(resolve(import.meta.dirname, "./utils/diffTemplate.html"));
+
+  return resolveHtmlTemplate(templateString, { ...diffModel, ...params });
 };
 
 export const diffEnvironmentsInternal = async (params: DiffEnvironmentsParams, commandName: string) => {
@@ -78,14 +82,10 @@ export const diffEnvironmentsInternal = async (params: DiffEnvironmentsParams, c
     params,
   );
 
-  const diffModel = diff({
+  return diff({
     targetAssetsReferencedFromSourceByCodenames: assetsReferences,
     targetItemsReferencedFromSourceByCodenames: itemReferences,
     targetEnvModel: transformedTargetModel,
     sourceEnvModel: sourceModel,
   });
-
-  return "advanced" in params
-    ? createAdvancedDiffFile({ ...diffModel, ...params })
-    : printDiff(diffModel, new Set(syncEntityChoices), params);
 };
