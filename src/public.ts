e85c81da2f4f319c1e03f54ca0399bddbf186da0
Ivan Kiral <kiralivan@gmail.com>
2024-10-15 14:14:10 +0200

    Fix typo in SyncDiffParams Type

--- src/public.ts
+++ src/public.ts
@@ -7,7 +7,7 @@ export { MigrationModule, MigrationOrder } from "./modules/migrations/models/mig
 export { MigrationStatus, ReadStatus, SaveStatus, Status } from "./modules/migrations/models/status.js";
 export { RunMigrationFilterParams, runMigrations, RunMigrationsParams } from "./modules/migrations/run.js";
 
-export { syncDiff, syncDiffParams } from "./modules/sync/diffEnvironments.js";
+export { syncDiff, SyncDiffParams } from "./modules/sync/diffEnvironments.js";
 export { syncExport, SyncExportParams } from "./modules/sync/syncModelExport.js";
 export { SyncEntities, syncRun, SyncRunParams } from "./modules/sync/syncRun.js";
 export {
