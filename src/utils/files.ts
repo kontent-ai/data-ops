b71afeb585a098dcec7778f2e34f9f3c0d8f6e79
Ivan Kiral <kiralivan@gmail.com>
2024-12-06 10:13:00 +0100

    fix filename serialization for backup command

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,4 +1,18 @@
-export const serializeDateForFileName = (date: Date) =>
-  `${date.getUTCFullYear()}-${
-    date.getUTCMonth() + 1
-  }-${date.getUTCDate()}-${date.getUTCHours()}-${date.getUTCMinutes()}`;
+export enum DateLevel {
+  Year = 0,
+  Month = 1,
+  Day = 2,
+  Hour = 3,
+  Minute = 4,
+  Second = 5,
+}
+
+export const serializeDateForFileName = (date: Date, level: DateLevel) =>
+  [
+    date.getUTCFullYear(),
+    ...level >= DateLevel.Month ? [("0" + (date.getUTCMonth() + 1)).slice(-2)] : [],
+    ...level >= DateLevel.Day ? [("0" + date.getUTCDate()).slice(-2)] : [],
+    ...level >= DateLevel.Hour ? [("0" + date.getUTCHours()).slice(-2)] : [],
+    ...level >= DateLevel.Minute ? [("0" + date.getUTCMinutes()).slice(-2)] : [],
+    ...level >= DateLevel.Second ? [("0" + date.getUTCSeconds()).slice(-2)] : [],
+  ].join("-");
